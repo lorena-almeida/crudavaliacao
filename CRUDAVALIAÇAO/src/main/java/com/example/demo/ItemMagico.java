df3b3e457f83c5f365e05539f52efda01c56e843
Lorena Almeida <79064100+lorena-almeida@users.noreply.github.com>
2025-04-08 22:06:38 -0300

    Add files via upload

---
+++ CRUDAVALIAÇAO/src/main/java/com/example/demo/ItemMagico.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+
+public class ItemMagico {
+    public int ID;
+    public char Nome;
+    public boolean tipoItem;
+    public int Forca;
+    public int Defesa;
+}
---
+++ CRUDAVALIAÇAO/src/main/java/com/example/demo/Personagem.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Personagem {
+    public char Nome;
+    public char nomeAventureiro;
+    public boolean Classe;
+    public int Level;
+    public char listaItens;
+    public int Forca;
+    public int Defesa;
+
+
+
+}
